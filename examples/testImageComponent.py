5c4d1271448bd6ddcd75fed21c62e4dc59aeebe1
AnthonyMeilland <63512040+AnthonyMeilland@users.noreply.github.com>
2022-09-07 17:32:18 +0200

    Update testImageComponent.py

--- examples/testImageComponent.py
+++ examples/testImageComponent.py
@@ -7,22 +7,23 @@ import astropy.units as u
 mas2rad=u.mas.to(u.rad)
 
 # Create some spatial frequencies (Baselines from 0 to 400m at 2.1 microns)
+npts=1000
 wl=0.65e-6
-B=np.linspace(1e-10,100,num=10000)
+B=np.linspace(1e-10,100,num=npts)
 spf=B/wl
 spf0=spf*0
 
 
 
-
 # Creating a Uniform disk image 
-dim=2048
+dim=800
 pixSize=0.1 #mas
 D=10 #mas
 xx=np.tile((np.arange(dim)-dim/2)*pixSize,(dim,1))
 yy=np.transpose(xx)
 data=((xx**2+yy**2)<(D/2)**2).astype(float)
-#data=data/np.sum(data) 
+data=data/np.sum(data) 
+
 
 # A few quantities useful to know if the object is well sampled
 
@@ -37,23 +38,18 @@ print("Baseline sampling with fov={}mas: {:.1f}m".format(fov,dB))
 
 
 
-ud0=oim.oimUD(f=0.2,d=2)
+ud0=oim.oimUD(f=0.2,d=2,x=10)
 
 # Building the model with the direct image of the UD  
-imUD=oim.oimComponentImage(pixSize=pixSize,x=10)
+imUD=oim.oimComponentImage(pixSize=pixSize)
 imUD.setImage(data)
-imUD.zpfact=1
 modelDirect=oim.oimModel([imUD,ud0])
 
 # Building the model with the FT analytical component
-ud=oim.oimUD(d=10,x=10)
+ud=oim.oimUD(d=10)#,x=10)
 modelFT=oim.oimModel([ud,ud0])
 
-
-
-
 vDirect=np.abs(modelDirect.getComplexCoherentFlux(spf,spf0))
-
 
 vFT=np.abs(modelFT.getComplexCoherentFlux(spf,spf0)) 
 
@@ -82,19 +78,18 @@ ax[1].set_ylim(-5*mresiduals,5*mresiduals)
 # Create images of the models  from their FT formulas
 
 normPow=1
-dim=1024   #Number of pixel for the image
-pix=0.1  #size of the pixel in the image in mas
 fig,ax=plt.subplots(1,2,figsize=(10,5))
 
 names=["2 FT UDs","Image UD + FT UD"]
 for i,mi in enumerate([modelDirect,modelFT]):
 
-    im=mi.getImage(dim,pix,fromFT=True)
+    im=mi.getImage(dim,pixSize,fromFT=True)
     im=im/np.sum(im)
-    ax[i].imshow(im,extent=[dim/2*pix,-dim/2*pix,-dim/2*pix,dim/2*pix],
-               norm=colors.PowerNorm(gamma=normPow))
+    ax[i].imshow(im,extent=[dim/2*pixSize,-dim/2*pixSize,-dim/2*pixSize,
+                            dim/2*pixSize],norm=colors.PowerNorm(gamma=normPow))
     ax[i].set_xlabel("$\\alpha$(mas)")    
     
-    ax[i].text(0,0.4*dim*pix,names[i],color="w",va="top",ha="center",fontSize=15)
+    ax[i].text(0,0.4*dim*pixSize,names[i],color="w",va="top",
+               ha="center",fontsize=15)
     
 ax[0].set_ylabel("$\\delta$(mas)")
