a80ede7fdca06ffa1a318ebaceb60478a8e7678b
AnthonyMeilland <63512040+AnthonyMeilland@users.noreply.github.com>
2022-09-16 01:19:55 +0200

    Create oimFilter.py

---
+++ oimodeler/oimFilter.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  8 13:21:23 2022
+
+@author: Ame
+"""
+
+import numpy as np
+from astropy.io import fits
+import os
+import oimodeler as oim
+
+
+
+###############################################################################
+ 
