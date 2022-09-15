da8c596e04d5b0dd2c4072cab8fc98bce3afc261
AnthonyMeilland <63512040+AnthonyMeilland@users.noreply.github.com>
2022-09-16 01:18:28 +0200

    testPlot example modifs

--- examples/BasicExamples/testPlots.py
+++ examples/BasicExamples/testPlots.py
@@ -10,27 +10,51 @@ import os
 from astropy.io import fits
 import oimodeler as oim
 
+path = os.path.dirname(oim.__file__)
+pathData=os.path.join(path,os.pardir,"examples","testData","ASPRO_MATISSE2")
 
-
-path="D:\\Travail\\GitHub\\oimodeler\\examples\\testData\\FSCMa_MATISSE\\"
-files=[os.path.abspath(os.path.join(path,fi)) for fi in os.listdir(path) if ".fits" in fi]
+files=[os.path.abspath(os.path.join(pathData,fi)) for fi in os.listdir(pathData) if ".fits" in fi]
 data=[fits.open(fi,mode="update") for fi in files]
 
 
+#%%
+# using the projection='oimAsxes' for all subpltos allow to use oimPlot custom plots
 fig, ax = plt.subplots(2,2, subplot_kw=dict(projection='oimAxes'),figsize=(8,8))
 
+#First we plot the uv plan coverage 
 ax[0,0].uvplot(data)
-lamcol=ax[0,1].oiplot(data,"SPAFREQ","VIS2DATA" ,xunit="cycles/mas",
+
+#Then we plot the V² as a function of spa. freq. with a wavelength-colorscale  
+lamcol=ax[0,1].oiplot(data,"SPAFREQ","VIS2DATA" ,xunit="cycles/mas",label="Data",
                     cname="EFF_WAVE",cunitmultiplier=1e6,ls=":",errorbar=True)
+
+#Adding the corresponding colobar of the walvength colorscale
 fig.colorbar(lamcol, ax=ax[0,1],label="$\\lambda$ ($\mu$m)")
 
-ax[1,0].oiplot(data,"EFF_WAVE","VIS2DATA",xunitmultiplier=1e6,errorbar=True,kwargs_error={"alpha":0.1})
-ax[1,1].oiplot(data,"SPAFREQ","T3PHI",xunit="cycles/mas",errorbar=True,lw=2,ls=":",colorTab=["red","green","blue"])
+#Legending also work with multicolor plots
+ax[0,1].legend()
+
+# Plotting the V² as a function of the wavlength with errorbars 
+#kwargs_error keyword allows to pass keywords to the errorbar plot
+ax[1,0].oiplot(data,"EFF_WAVE","VIS2DATA",xunitmultiplier=1e6,
+               errorbar=True,kwargs_error={"alpha":0.1})
 
 
+# Finally we plot the Closure Phase with a fewstyling options
+ax[1,1].oiplot(data,"SPAFREQ","T3PHI",xunit="cycles/mas",errorbar=True,
+               lw=2,ls=":")
+
+#setting log scale as for normal matplotlib plots
 ax[0,1].set_yscale('log')
-#ax[1].autolim()
+
+#autolim allows to directly set ylim to (0,1) for vis and (-180,180) for phase
 ax[1,0].autolim()
 ax[1,1].autolim()
+
 fig.tight_layout()     
 
+
+#%%
+#Saving the plot
+filename=os.path.join(path,os.pardir,"images","oimodel_test_plots.png")
+plt.savefig(filename)
