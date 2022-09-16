5f658cb211bc88c2734421ceccd6e5551360d546
AnthonyMeilland <63512040+AnthonyMeilland@users.noreply.github.com>
2022-09-16 17:19:17 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,7 +10,7 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
+import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
@@ -20,7 +20,7 @@
 import pathlib
 import sys
 sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
-
+sys.path.insert(0, os.path.abspath('..'))
 
 
 # -- Project information -----------------------------------------------------
