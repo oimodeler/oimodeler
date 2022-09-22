1528360107fdd74a651c04fec38c2c2ae61254bc
AnthonyMeilland <63512040+AnthonyMeilland@users.noreply.github.com>
2022-09-22 16:05:36 +0200

    Update simpleFitEmcee.py

--- examples/BasicExamples/simpleFitEmcee.py
+++ examples/BasicExamples/simpleFitEmcee.py
@@ -6,13 +6,7 @@ Created on Wed Sep 21 10:59:15 2022
 """
 
 import oimodeler as oim
-import matplotlib.pyplot as plt
-import numpy as np
-import astropy.units as u
 import os
-from datetime import datetime
-from astropy.io import fits
-import corner.corner
 
 path = os.path.dirname(oim.__file__)
 
@@ -25,6 +19,8 @@ ud=oim.oimUD(d=3,f=0.5)
 pt=oim.oimPt(f=1)
 model=oim.oimModel([ud,pt])
 
+#%%
+
 #setting limits of the parameter space but also setting x,y of the UD as a 
 #free parameters and f of the pt as fixed (to 1)
 ud.params['d'].set(min=0.01,max=20)
@@ -33,6 +29,8 @@ ud.params['y'].set(min=-50,max=50,free=True)
 ud.params['f'].set(min=0.,max=10.)
 pt.params['f'].free=False
 
+
+#%%
 #Create a new fitter with 32 walkers and the list of oifits files and the model
 fit=oim.oimFitterEmcee(files,model,nwalkers=32)
 #print(fit._logProbability([0,10,1,5]))
@@ -50,15 +48,36 @@ print(fit.initialParams)
 fit.run(nsteps=10000,progress=True)
 
 
+#%% Getting results from the mcmc run
+# Result method return the models values and 1 sigma uncertainties 
+#(low, up and avg) computed from the distrib qnuatiles (0.16,0.84)
+#It also compute simulated Data with the best/mean/median model
+
+# Results can be computed with various options : 
+    
+#Best return the  model with the lowest chi2
+best,err_l,err_u,err=fit.getResults(mode='best')
+
+#Mean return the mean value of the parameters excluding first "discard" steps
+# and models with a chi2 >chi2min*chi2limifact 
+mean,err_l,err_u,err=fit.getResults(mode='mean',discard=3000,chi2limfact=20)
+
+#Median return the median model
+median,err_l,err_u,err=fit.getResults(mode='median',discard=2000,chi2limfact=20)
+
+#%% plotting corner and walkers plots and saving them
+figCorner,axeCorner=fit.cornerPlot(discard=2000,savefig=
+                os.path.join(path,os.pardir,"images","SimpleFitCorner.png"))
+
+figWalkers,axeWalkers=fit.walkersPlot(cmap="plasma_r",savefig=
+                os.path.join(path,os.pardir,"images","SimpleFitWalkers.png"))
+
+
 #%%
-best,err_m,err_p,err=fit.getResults(mode='best')
-mean,err_m,err_p,err=fit.getResults(mode='mean',discard=2000,chi2limfact=10)
-median,err_m,err_p,err=fit.getResults(mode='median',discard=2000,chi2limfact=10)
+#Plotting the median model (last computed) over the data
+fit.simulator.compute(computeChi2=True,computeSimulatedData=True)
+figSim,axSim=fit.simulator.plot(["VIS2DATA","VISAMP","VISPHI","T3AMP","T3PHI"])
 
-#%% Saving corner and walkers plots
-figCorner,axeCorner=fit.cornerPlot(discard=2000)#,savefig=
-               #os.path.join(path,os.pardir,"images","SimpleFitCorner.png"))
+median,err_l,err_u,err=fit.getResults(mode='median',discard=1000,chi2limfact=20)
 
-figWalkers,axeWalkers=fit.walkersPlot(cmap="plasma_r")#,savefig=
-                #os.path.join(path,os.pardir,"images","SimpleFitWalkers.png"))
 
