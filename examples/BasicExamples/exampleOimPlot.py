e6d7117e994e5405666669ffa348585756745f3e
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2023-03-13 18:03:18 +0100

    Reformat BasicExamples

--- examples/BasicExamples/exampleOimPlot.py
+++ examples/BasicExamples/exampleOimPlot.py
@@ -10,77 +10,78 @@ import matplotlib.pyplot as plt
 import oimodeler as oim
 
 path = os.path.dirname(oim.__file__)
-pathData=os.path.join(path,os.pardir,"examples","testData","ASPRO_MATISSE2")
+pathData = os.path.join(path, os.pardir, "examples",
+                        "testData", "ASPRO_MATISSE2")
 
-files=[os.path.abspath(os.path.join(pathData,fi)) for fi in os.listdir(pathData) if ".fits" in fi]
-data=oim.oimData(files)
+files = [os.path.abspath(os.path.join(pathData, fi))
+         for fi in os.listdir(pathData) if ".fits" in fi]
+data = oim.oimData(files)
 
 
-#%%
+# %%
 fig1 = plt.figure()
 ax1 = plt.subplot(projection='oimAxes')
 ax1.uvplot(data)
-plt.savefig(os.path.join(path,os.pardir,"images","ExampleOimPlot_uv.png"))
+plt.savefig(os.path.join(path, os.pardir, "images", "ExampleOimPlot_uv.png"))
 
-#%%
+# %%
 fig2 = plt.figure()
 ax2 = plt.subplot(projection='oimAxes')
-lamcol=ax2.oiplot(data,"SPAFREQ","VIS2DATA" ,xunit="cycles/mas",label="Data",
-                cname="EFF_WAVE",cunitmultiplier=1e6,errorbar=True)
-                
-plt.colorbar(lamcol, ax=ax2,label="$\\lambda$ ($\mu$m)")
+lamcol = ax2.oiplot(data, "SPAFREQ", "VIS2DATA", xunit="cycles/mas", label="Data",
+                    cname="EFF_WAVE", cunitmultiplier=1e6, errorbar=True)
+
+plt.colorbar(lamcol, ax=ax2, label="$\\lambda$ ($\mu$m)")
 ax2.legend()
 
-plt.savefig(os.path.join(path,os.pardir,"images","ExampleOimPlot_v2.png"))
+plt.savefig(os.path.join(path, os.pardir, "images", "ExampleOimPlot_v2.png"))
 
-#%%
-fig3= plt.figure()
+# %%
+fig3 = plt.figure()
 ax3 = plt.subplot(projection='oimAxes')
-ax3.oiplot(data,"EFF_WAVE","VIS2DATA",xunitmultiplier=1e6,color="byConfiguration",
-               errorbar=True,kwargs_error={"alpha":0.3})
+ax3.oiplot(data, "EFF_WAVE", "VIS2DATA", xunitmultiplier=1e6, color="byConfiguration",
+           errorbar=True, kwargs_error={"alpha": 0.3})
 ax3.legend()
 
-plt.savefig(os.path.join(path,os.pardir,"images","ExampleOimPlot_v2Wl.png"))
+plt.savefig(os.path.join(path, os.pardir, "images", "ExampleOimPlot_v2Wl.png"))
 
-#%%
 # using the projection='oimAsxes' for all subpltos allow to use oimPlot custom plots
-fig4, ax4 = plt.subplots(2,2, subplot_kw=dict(projection='oimAxes'),figsize=(8,8))
+fig4, ax4 = plt.subplots(2, 2, subplot_kw=dict(
+    projection='oimAxes'), figsize=(8, 8))
 
-#First we plot the uv plan coverage 
-ax4[0,0].uvplot(data)
+# First we plot the uv plan coverage
+ax4[0, 0].uvplot(data)
 
-#Then we plot the V² as a function of spa. freq. with a wavelength-colorscale  
-lamcol=ax4[0,1].oiplot(data,"SPAFREQ","VIS2DATA" ,xunit="cycles/mas",label="Data",
-                    cname="EFF_WAVE",cunitmultiplier=1e6,ls=":",errorbar=True)
+# Then we plot the V² as a function of spa. freq. with a wavelength-colorscale
+lamcol = ax4[0, 1].oiplot(data, "SPAFREQ", "VIS2DATA", xunit="cycles/mas", label="Data",
+                          cname="EFF_WAVE", cunitmultiplier=1e6, ls=":", errorbar=True)
 
-#Adding the corresponding colobar of the walvength colorscale
-fig4.colorbar(lamcol, ax=ax4[0,1],label="$\\lambda$ ($\mu$m)")
+# Adding the corresponding colobar of the walvength colorscale
+fig4.colorbar(lamcol, ax=ax4[0, 1], label="$\\lambda$ ($\mu$m)")
 
-#Legending also work with multicolor plots
-ax4[0,1].legend()
+# Legending also work with multicolor plots
+ax4[0, 1].legend()
 
-# Plotting the V² as a function of the wavlength with errorbars 
-#kwargs_error keyword allows to pass keywords to the errorbar plot
-ax4[1,0].oiplot(data,"EFF_WAVE","VIS2DATA",xunitmultiplier=1e6,color="byBaseline",
-               errorbar=True,kwargs_error={"alpha":0.1})
-ax4[1,0].legend(fontsize=6)
+# Plotting the V² as a function of the wavlength with errorbars
+# kwargs_error keyword allows to pass keywords to the errorbar plot
+ax4[1, 0].oiplot(data, "EFF_WAVE", "VIS2DATA", xunitmultiplier=1e6, color="byBaseline",
+                 errorbar=True, kwargs_error={"alpha": 0.1})
+ax4[1, 0].legend(fontsize=6)
 
 
 # Finally we plot the Closure Phase with a fewstyling options
-ax4[1,1].oiplot(data,"SPAFREQ","T3PHI",xunit="cycles/mas",errorbar=True,
-               lw=2,ls=":",color="byFile")
-ax4[1,1].legend(fontsize=4)
+ax4[1, 1].oiplot(data, "SPAFREQ", "T3PHI", xunit="cycles/mas", errorbar=True,
+                 lw=2, ls=":", color="byFile")
+ax4[1, 1].legend(fontsize=4)
 
 
+# setting log scale as for normal matplotlib plots
+ax4[0, 1].set_yscale('log')
 
-#setting log scale as for normal matplotlib plots
-ax4[0,1].set_yscale('log')
+# autolim allows to directly set ylim to (0,1) for vis and (-180,180) for phase
+ax4[1, 0].autolim()
+ax4[1, 1].autolim()
 
-#autolim allows to directly set ylim to (0,1) for vis and (-180,180) for phase
-ax4[1,0].autolim()
-ax4[1,1].autolim()
+fig4.tight_layout()
 
-fig4.tight_layout()     
-
-filename=os.path.join(path,os.pardir,"images","ExampleOimPlot_multi.png")
+filename = os.path.join(path, os.pardir, "images", "ExampleOimPlot_multi.png")
 plt.savefig(filename)
--- examples/BasicExamples/fitsImageModel.py
+++ examples/BasicExamples/fitsImageModel.py
@@ -12,92 +12,90 @@ import oimodeler as oim
 from astropy.io import fits
 from matplotlib import pyplot as plt
 
-#You can change FFT option, for instance reduce the standard zero-padding factor 
-#from 8 to 2 or use FFTW backend instead of the standard numpy FFT module
-#oim.oimOptions['FTpaddingFactor']=2
-#oim.oimOptions['FTBackend']=oim.FFTWBackend
-
-
+# You can change FFT option, for instance reduce the standard zero-padding factor
+# from 8 to 2 or use FFTW backend instead of the standard numpy FFT module
+# oim.oimOptions['FTpaddingFactor']=2
+# oim.oimOptions['FTBackend']=oim.FFTWBackend
 path = os.path.dirname(oim.__file__)
-pathData=os.path.join(path,os.pardir,"examples","BasicExamples")
-filename=os.path.join(pathData,"BeDISCO.fits")
+pathData = os.path.join(path, os.pardir, "examples", "BasicExamples")
+filename = os.path.join(pathData, "BeDISCO.fits")
 
-im=fits.open(filename)
+im = fits.open(filename)
 
-#%% creating the model
+# %% creating the model
+im = fits.open(filename)
 
-im=fits.open(filename)
-c=oim.oimComponentFitsImage(im) # load image from an opened astropy.io.primaryHDU
-#c=oim.oimComponentFitsImage(filename) # load image from a valid filename of fits file
+# load image from an opened astropy.io.primaryHDU
+c = oim.oimComponentFitsImage(im)
 
-m=oim.oimModel(c)
+# c=oim.oimComponentFitsImage(filename) # load image from a valid filename of fits file
 
-#%% Plotting the model image
-m.showModel(512,0.05,legend=True,normalize=True,normPow=1,cmap="hot",figsize=(7,5.5),
-     savefig=os.path.join(path,os.pardir,"images","FitsImage_Disco_image.png"))
+m = oim.oimModel(c)
 
-#%%
-#Create some spatial frequencies (Baselines from 0 to 120m at 1.5 microns)
-wl=1.5e-6
-nB=1000
-B=np.linspace(0,120,num=nB)
+# %% Plotting the model image
+m.showModel(512, 0.05, legend=True, normalize=True, normPow=1, cmap="hot", figsize=(7, 5.5),
+            savefig=os.path.join(path, os.pardir, "images", "FitsImage_Disco_image.png"))
 
-spfx=np.append(B,B*0)/wl # 1st half of B array are baseline in the East-West orientation
-spfy=np.append(B*0,B)/wl # 2nd half are baseline in the North-South orientation
+# %%
+# Create some spatial frequencies (Baselines from 0 to 120m at 1.5 microns)
+wl = 1.5e-6
+nB = 1000
+B = np.linspace(0, 120, num=nB)
+
+# 1st half of B array are baseline in the East-West orientation
+spfx = np.append(B, B*0)/wl
+# 2nd half are baseline in the North-South orientation
+spfy = np.append(B*0, B)/wl
 
 
-
-ccf = m.getComplexCoherentFlux(spfx,spfy)
+ccf = m.getComplexCoherentFlux(spfx, spfy)
 v = np.abs(ccf)
-v=v/v.max()
-
+v = v/v.max()
 
 plt.figure()
-plt.plot(B , v[0:nB],label="East-West")
-plt.plot(B , v[nB:],label="North-South")
+plt.plot(B, v[0:nB], label="East-West")
+plt.plot(B, v[nB:], label="North-South")
 plt.xlabel("B (m)")
 plt.ylabel("Visbility")
 plt.legend()
 plt.margins(0)
 
-plt.savefig(os.path.join(path,os.pardir,"images",
+plt.savefig(os.path.join(path, os.pardir, "images",
                          "FitsImage_Disco_visibility.png"))
 
 
-#%%
+# %%
 print(m.getParameters())
 
-#%% scaling and rotating
+# %% scaling and rotating
+c.params['pa'].value = 40
+c.params['scale'].value = 0.8
 
-c.params['pa'].value=40
-c.params['scale'].value=0.8
+m.showModel(512, 0.05, legend=True, normalize=True, normPow=1, cmap="hot", figsize=(7, 5.5),
+            savefig=os.path.join(path, os.pardir, "images", "FitsImage_Disco_image2.png"))
 
-m.showModel(512,0.05,legend=True,normalize=True,normPow=1,cmap="hot",figsize=(7,5.5),
-     savefig=os.path.join(path,os.pardir,"images","FitsImage_Disco_image2.png"))
+# %%Adding a companion
 
-#%%Adding a companion
+c2 = oim.oimUD(x=20, d=1, f=0.03)
+m2 = oim.oimModel(c, c2)
 
-c2=oim.oimUD(x=20,d=1,f=0.03)
-m2=oim.oimModel(c,c2)
-
-m2.showModel(512,0.1,legend=True,normalize=True,fromFT=True,normPow=1,
-            cmap="hot",savefig=os.path.join(path,os.pardir,"images",
-                 "FitsImage_Disco_image3.png"))
+m2.showModel(512, 0.1, legend=True, normalize=True, fromFT=True, normPow=1,
+             cmap="hot", savefig=os.path.join(path, os.pardir, "images",
+                                              "FitsImage_Disco_image3.png"))
 
 
-#%%
-
-ccf = m2.getComplexCoherentFlux(spfx,spfy)
+# %%
+ccf = m2.getComplexCoherentFlux(spfx, spfy)
 v = np.abs(ccf)
-v=v/v.max()
+v = v/v.max()
 
 plt.figure()
-plt.plot(B , v[0:nB],label="East-West")
-plt.plot(B , v[nB:],label="North-South")
+plt.plot(B, v[0:nB], label="East-West")
+plt.plot(B, v[nB:], label="North-South")
 plt.xlabel("B (m)")
 plt.ylabel("Visbility")
 plt.legend()
 plt.margins(0)
 
-plt.savefig(os.path.join(path,os.pardir,"images",
+plt.savefig(os.path.join(path, os.pardir, "images",
                          "FitsImage_Disco_visibility2.png"))
