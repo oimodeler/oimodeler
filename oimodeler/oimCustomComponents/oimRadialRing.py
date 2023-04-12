640bda71e1ca1a91aa02bad2f8d0289f3e28b570
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2023-04-12 15:38:00 +0200

    Reformat and fix errors in new components

--- oimodeler/oimCustomComponents/oimRadialRing.py
+++ oimodeler/oimCustomComponents/oimRadialRing.py
@@ -1,86 +1,75 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Dec 14 14:39:36 2021
-
-@author: Ama
-"""
-
 import numpy as np
-from oimodeler import oimParam, oimComponentRadialProfile,_standardParameters
-from astropy import units as units
-import matplotlib.pyplot as plt
-import matplotlib.colors as colors
-import astropy.units as u
-import matplotlib.cm as cm
-from scipy.interpolate import interp1d
-import os
 
-mas2rad=u.mas.to(u.rad)
-dim = 256
+from ..oimParam import _standardParameters, oimParam
+from ..oimComponent import oimComponentRadialProfile
+
 
 class oimRadialRing(oimComponentRadialProfile):
-    name="A ring defined by a radial intensity profile in r^p"
-    shortname = "RadialRing"
-    
-    elliptic=True
-    
-    def __init__(self,**kwargs): 
-         # First call the __init__ function from the parent class
-         super().__init__(**kwargs)
-         # Then define the component parameters  
-         self.params["din"]=oimParam(_standardParameters["din"]) 
-         self.params["dout"]=oimParam(_standardParameters["dout"])
-         self.params["p"]=oimParam(name="p", value=0,description="Power-law exponent")
-         
-         self._dim=dim
-         
-         self._t = np.array([0,1e+99]) # constant value <=> static model
-         self._wl = None#np.array([0.5,1])*1e-6
-         
-         # Finally call the _eval function that allow the parameters to be processed
-         self._eval(**kwargs)
-         
-    
-    def _radialProfileFunction(self,r,wl,t):
-        #r=np.arange(0,dim)*pixSize*mas2rad
-        din=self.params["din"](wl,t)
-        dout=self.params["dout"](wl,t)
-        p=self.params["p"](wl,t)
-        #r=np.linspace(din/2.,dout/2.,dim)
-        rin=din/2.
-        rout=dout/2.
-        I_in=1.
-        I=np.nan_to_num(np.logical_and(r>rin,r<rout).astype(int)*I_in*np.divide(r,rin)**p,nan=0)
-        #I=np.nan_to_num((r>rin).astype(float)*np.exp(-0.692*np.divide(r-rin,rout)),nan=0)
-    #I=np.nan_to_num((r>r0).astype(float)*np.exp(-0.692*np.divide(r-r0,fwhm)),nan=0)
-        #I=I_in*(r/rin)**p
-        #I=(r<=self.params["d"].value*self.params["d"].unit.to(units.rad)/2).astype(float)
-        return I
-    
+    """A ring defined by a radial intensity profile in r^p.
+
+    It accounts for elongation and rotation
+
+    Parameters
+    ----------
+    din : u.mas
+        Inner radius of the disk
+    dout : u.mas
+        Outer radius of the disk
+    p : u.one
+        Power-law exponent for the radial profile
+    pa : u.deg
+        Positional angle
+    elong : float
+        Elongation of the disk
+    dim : u.one
+        Dimension of the image
+
+    Attributes
+    ----------
+    params : Dict[str, oimParam]
+        Dictionary of parameters
+    _t : np.ndarray
+        Array of time values
+    _wl : np.ndarray
+        Array of wavelength values
+    _r : np.ndarray
+
+    Methods
+    -------
+    _radialProfileFunction(xx, yy, wl, t)
+        Calculates a radial power law profile
+    """
+    name = "Radial Ring"
+    shortname = "RadRing"
+    elliptic = True
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.params["din"] = oimParam(**_standardParameters["din"])
+        self.params["dout"] = oimParam(**_standardParameters["dout"])
+        self.params["p"] = oimParam(**_standardParameters["p"])
+
+        self._t = np.array([0])  # constant value <=> static model
+        self._wl = np.array([0])  # constant value <=> achromatic model
+        self._eval(**kwargs)
+
+    def _radialProfileFunction(self, r, wl, t):
+        """Calculates a radial power law profile"""
+        p = self.params["p"](wl, t)
+        rin, rout = map(lambda x: self.params[x](wl, t)/2, ("din", "dout"))
+        return np.nan_to_num(np.logical_and(r > rin, r < rout).astype(int)*(r / rin)**p, nan=0)
+
     @property
     def _r(self):
+        """Gets the radius"""
         if False:
-            dout=self.params["dout"](self._wl,self._t)
+            dout = self.params["dout"](self._wl, self._t)
         else:
-            dout=self.params["dout"](1e99)
-        rmax=4*dout/2.
-        r=np.linspace(0,1, self._dim)*rmax
-        return r
-        #din=self.params["din"](self._wl,self._t)* mas2rad
-        #dout=self.params["dout"](self._wl,self._t)* mas2rad
-        #r=np.linspace(din/2.,dout/2.,self._dim)
-        #return r
+            dout = self.params["dout"](1e99)
+        rmax = 4*dout/2.
+        return np.linspace(0, 1, self._dim)*rmax
 
     @_r.setter
     def _r(self, r):
+        """Sets the radius"""
         pass
-    
-    # def _imageFunction(self,xx,yy,wl,t):
-    #     r2=(xx**2+yy**2)
-    #     I_in=1.
-    #     Ir=I_in*(np.sqrt(r2)/self.params["din"](wl,t)/2)**self.params["p"](wl,t)
-    #     return  ((r2<=(self.params["dout"](wl,t)/2)**2) & 
-    #              (r2>=(self.params["din"](wl,t)/2)**2)).astype(float)*Ir
-
-
-############################################################################### 
--- oimodeler/oimCustomComponents/oimTempGradient.py
+++ oimodeler/oimCustomComponents/oimTempGradient.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Dec 14 14:39:36 2021
-
-@author: Ama
-"""
 import astropy.units as u
 import astropy.constants as const
 import numpy as np
@@ -70,16 +64,17 @@ class oimTempGradient(oimComponentRadialProfile):
     ----------
     params : Dict[str, oimParam]
         Dictionary of parameters
-    _t : np.array
+    _t : np.ndarray
         Array of time values
-    _wl : np.array
+    _wl : np.ndarray
         Array of wavelength values
-    _r : np.array
+    _r : np.ndarray
 
     Methods
     -------
     _radialProfileFunction(r, wl, t)
-        Radial profile function
+        Calculates a radial temperature gradient profile via a dust-surface
+        density- and temperature profile
     """
     name = "Temperature Gradient"
     shortname = "TempGrad"
@@ -95,12 +90,11 @@ class oimTempGradient(oimComponentRadialProfile):
                                     description="Power-law exponent for the temperature profile")
         self.params["p"] = oimParam(name="p", value=0, unit=u.one,
                                     description="Power-law exponent for the dust surface density profile")
+        self.params["Mdust"] = oimParam(name="Mdust", value=0, unit=u.M_sun,
+                                        description="Mass of the dusty disk")
         self.params["Tin"] = oimParam(name="Tin", value=0,
                                       unit=u.K, free=False,
                                       description="Inner radius temperature")
-        self.params["Mdust"] = oimParam(name="Mdust", value=0,
-                                        unit=const.M_sun, free=False,
-                                        description="Mass of the dusty disk")
         self.params["kappa_abs"] = oimParam(name="kappa_abs", value=0,
                                             unit=u.cm**2/u.g, free=False,
                                             description="Dust mass absorption coefficient")
@@ -108,48 +102,53 @@ class oimTempGradient(oimComponentRadialProfile):
                                        unit=u.pc, free=False,
                                        description="Distance of the star")
 
-        self._t = np.array([0, 1e+99])  # constant value <=> static model
-        self._wl = None  # np.array([0.5,1])*1e-6
+        self._t = np.array([0])  # constant value <=> static model
+        self._wl = np.array([0])  # constant value <=> achromatic model
         self._eval(**kwargs)
 
     def _radialProfileFunction(self, r, wl, t):
+        """Calculates a radial temperature gradient profile via a dust-surface
+        density- and temperature profile"""
         rin, rout = map(lambda x: self.params[x](wl, t), ["rin", "rout"])
         q, p = map(lambda x: self.params[x](wl, t), ["q", "p"])
-        dist, Tin = map(lambda x: self.params[x](wl, t), ["dist", "Tin"])
-        M_sun, c, h, k = const.M_sun.value, const.c.value, const.h.value, const.k_B.value
-        Mdust = self.params["Mdust"](wl, t)*M_sun*1e3
+        dist, inner_temp = map(lambda x: self.params[x](wl, t), ["dist", "Tin"])
+        dust_mass = self.params["Mdust"](wl, t)*const.M_sun.value*1e3
         kappa_abs = self.params["kappa_abs"](wl, t)
 
         rin_mas, rout_mas = map(lambda x: 1e3*x/dist, [rin, rout])
-        rin_cm, rout_cm = map(
-            lambda x: x*self.params["rin"].unit.to(u.cm), [rin, rout])
+        rin_cm, rout_cm = map(lambda x: x*self.params["rin"].unit.to(u.cm), [rin, rout])
 
         # NOTE: Temperature radial profile
-        temp_profile = Tin*(r / rin_mas)**q
+        temp_profile = inner_temp*(r / rin_mas)**q
 
         # NOTE: Surface density radial profile
         if p == -2:
-            sigma_in = Mdust/(2.*np.pi*np.log(rout_cm/rin_cm)*rin_cm**2)
+            sigma_in = dust_mass/(2.*np.pi*np.log(rout_cm/rin_cm)*rin_cm**2)
         else:
             f = ((rout_cm/rin_cm)**(p+2)-1)/(p+2)
-            sigma_in = Mdust/(2.*np.pi*f*rin_cm**2)
-        sigma_profile = sigma_in*np.divide(r, rin_mas)**p
+            sigma_in = dust_mass/(2.*np.pi*f*rin_cm**2)
+        sigma_profile = sigma_in*(r / rin_mas)**p
         spectral_density = calculate_spectral_density(wl, kappa_abs,
                                                       temp_profile, sigma_profile)
 
-        # NOTE: Final intensity profile
         return np.nan_to_num(np.logical_and(r > rin_mas, r < rout_mas).astype(int)*spectral_density, nan=0)
 
     @property
     def _r(self):
+        """Gets the radius"""
         if False:
             rout = self.params["rout"](self._wl, self._t)
         else:
-            rout = self.params["rout"](1e99)
+            rout = self.params["rout"](self._wl, 1e99)
         rmax = 4*rout
-        r = np.linspace(0, 1, self._dim)*1e+3*rmax / \
+        r = np.linspace(0, 1, self.params["dim"](self._wl, self._t))*1e+3*rmax / \
             self.params["dist"](self._wl, self._t)
         return r
+
+    @_r.setter
+    def _r(self, value):
+        """Sets the radius"""
+        pass
 
 
 class oimAsymTempGradient(oimAsymRadialPowerLaw):
@@ -191,16 +190,16 @@ class oimAsymTempGradient(oimAsymRadialPowerLaw):
     ----------
     params : Dict[str, oimParam]
         Dictionary of parameters
-    _t : np.array
+    _t : np.ndarray
         Array of time values
-    _wl : np.array
+    _wl : np.ndarray
         Array of wavelength values
-    _r : np.array
 
     Methods
     -------
     _imageFunction(xx, yy, wl, t)
-        Image function
+        Calculates a 2D temperature gradient profile via the dust-surface
+        density- and temperature profile
     """
     name = "Asymmetric Temperature Gradient"
     shortname = "AsymTempGrad"
@@ -215,12 +214,11 @@ class oimAsymTempGradient(oimAsymRadialPowerLaw):
                                     description="Power-law exponent for the temperature profile")
         self.params["p"] = oimParam(name="p", value=0, unit=u.one,
                                     description="Power-law exponent for the dust surface density profile")
+        self.params["Mdust"] = oimParam(name="Mdust", value=0, unit=u.M_sun,
+                                        description="Mass of the dusty disk")
         self.params["Tin"] = oimParam(name="Tin", value=0,
                                       unit=u.K, free=False,
                                       description="Inner radius temperature")
-        self.params["Mdust"] = oimParam(name="Mdust", value=0,
-                                        unit=const.M_sun, free=False,
-                                        description="Mass of the dusty disk")
         self.params["kappa_abs"] = oimParam(name="kappa_abs", value=0,
                                             unit=u.cm**2/u.g, free=False,
                                             description="Dust mass absorption coefficient")
@@ -228,37 +226,35 @@ class oimAsymTempGradient(oimAsymRadialPowerLaw):
                                        unit=u.pc, free=False,
                                        description="Distance of the star")
 
-        self._t = np.array([0, 1e+99])  # constant value <=> static model
-        self._wl = None  # np.array([0.5,1])*1e-6
+        self._t = np.array([0])  # constant value <=> static model
+        self._wl = np.array([0])  # constant value <=> achromatic model
         self._eval(**kwargs)
 
     def _imageFunction(self, xx, yy, wl, t):
+        """Calculates a 2D temperature gradient profile via the dust-surface
+        density- and temperature profile"""
         r = np.sqrt(xx**2+yy**2)
         rin, rout = map(lambda x: self.params[x](wl, t), ["rin", "rout"])
         q, p = map(lambda x: self.params[x](wl, t), ["q", "p"])
-        dist, Tin = map(lambda x: self.params[x](wl, t), ["dist", "Tin"])
-        M_sun, c, h, k = const.M_sun.value, const.c.value, const.h.value, const.k_B.value
-        Mdust = self.params["Mdust"](wl, t)*M_sun*1e3
+        dist, inner_temp = map(lambda x: self.params[x](wl, t), ["dist", "Tin"])
+        dust_mass = self.params["Mdust"](wl, t)*const.M_sun.value*1e3
         kappa_abs = self.params["kappa_abs"](wl, t)
-        self._pixSize = self.params["pixSize"](
-            wl, t)*self.params["pixSize"].unit.to(u.rad)
+        self._pixSize = self.params["pixSize"](wl, t)*self.params["pixSize"].unit.to(u.rad)
 
         rin_mas, rout_mas = map(lambda x: 1e3*x/dist, [rin, rout])
-        rin_cm, rout_cm = map(
-            lambda x: x*self.params["rin"].unit.to(u.cm), [rin, rout])
+        rin_cm, rout_cm = map(lambda x: x*self.params["rin"].unit.to(u.cm), [rin, rout])
 
         # NOTE: Temperature radial profile
-        temp_profile = Tin*(r / rin_mas)**q
+        temp_profile = inner_temp*(r / rin_mas)**q
 
         # NOTE: Surface density radial profile
         if p == -2:
-            sigma_in = Mdust/(2.*np.pi*np.log(rout_cm/rin_cm)*rin_cm**2)
+            sigma_in = dust_mass/(2.*np.pi*np.log(rout_cm/rin_cm)*rin_cm**2)
         else:
             f = ((rout_cm/rin_cm)**(p+2)-1)/(p+2)
-            sigma_in = Mdust/(2.*np.pi*f*rin_cm**2)
-        sigma_profile = sigma_in*np.divide(r, rin_mas)**p
+            sigma_in = dust_mass/(2.*np.pi*f*rin_cm**2)
+        sigma_profile = self._azimuthal_modulation(xx, yy, wl, t)\
+            * sigma_in*(r / rin_mas)**p
         spectral_density = calculate_spectral_density(wl, kappa_abs,
                                                       temp_profile, sigma_profile)
-
-        # NOTE: Final intensity profile
         return np.nan_to_num(np.logical_and(r > rin_mas, r < rout_mas).astype(int)*spectral_density, nan=0)
