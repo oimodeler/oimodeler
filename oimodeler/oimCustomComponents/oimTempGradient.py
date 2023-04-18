2f54c9fde0f4d84ce18f5ae449399dc8cf8ea093
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2023-04-18 14:31:23 +0200

    Change RadialPowerLaw and temperature Gradient

--- oimodeler/oimCustomComponents/oimTempGradient.py
+++ oimodeler/oimCustomComponents/oimTempGradient.py
@@ -7,7 +7,7 @@ from astropy.modeling import models
 
 from ..oimComponent import oimComponentRadialProfile
 from ..oimParam import oimParam
-from .oimRadialPowerLaw import oimAsymRadialPowerLaw
+from .oimRadialPowerLaw import oimRadialPowerLaw
 
 
 def calculate_intensity(params: Dict[str, oimParam],
@@ -68,7 +68,7 @@ class oimTempGradient(oimComponentRadialProfile):
         Power-law exponent for the temperature profile
     p : float
         Power-law exponent for the dust surface density profile
-    kappa_abs : float
+    kappa_abs : float | oimInterp
         Dust mass absorption coefficient [cm2.g-1]
     dist : float
         Distance of the star [pc]
@@ -119,6 +119,7 @@ class oimTempGradient(oimComponentRadialProfile):
         self._wl = np.array([0])  # constant value <=> achromatic model
         self._eval(**kwargs)
 
+    # FIXME: Possible bug due to r being in mas or rad?
     def _radialProfileFunction(self, r, wl, t):
         """Calculates a radial temperature gradient profile via a dust-surface
         density- and temperature profile"""
@@ -165,16 +166,16 @@ class oimTempGradient(oimComponentRadialProfile):
         return
 
 
-class oimAsymTempGradient(oimAsymRadialPowerLaw):
+class oimAsymTempGradient(oimRadialPowerLaw):
     """A ring defined by a radial temperature profile in r^q and an asymmetric
     radial dust surface density profile in r^p
 
     Parameters
     ----------
     rin : float
-        Inner radius of the disk [au]
+        Inner radius of the disk [mas]
     rout : float
-        Outer radius of the disk [au]
+        Outer radius of the disk [mas]
     Tin : float
         Inner radius temperature [K]
     Mdust : float
@@ -182,22 +183,22 @@ class oimAsymTempGradient(oimAsymRadialPowerLaw):
     a : float
         Azimuthal modulation amplitude
     phi : float
-        Azimuthal modulation angle
+        Azimuthal modulation angle [deg]
     q : float
         Power-law exponent for the temperature profile
     p : float
         Power-law exponent for the dust surface density profile
-    kappa_abs : float
+    kappa_abs : float | oimInterp
         Dust mass absorption coefficient [cm2.g-1]
     dist : float
         Distance of the star [pc]
-    pixSize: u.mas/px
-        Pixel size
-    pa : u.deg
-        Positional angle
+    fov: float
+        The field of view [mas]
+    pa : float
+        Positional angle [deg]
     elong : float
         Elongation of the disk
-    dim : px
+    dim : float
         Dimension of the image
 
     Attributes
@@ -217,12 +218,14 @@ class oimAsymTempGradient(oimAsymRadialPowerLaw):
     """
     name = "Asymmetric Temperature Gradient"
     shortname = "AsymTempGrad"
+    elliptic = True
+    asymmetric = True
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.params["rin"] = oimParam(name="rin", value=0, unit=u.au,
+        self.params["rin"] = oimParam(name="rin", value=0, unit=u.mas,
                                       description="Inner radius of the disk")
-        self.params["rout"] = oimParam(name="rout", value=0, unit=u.au,
+        self.params["rout"] = oimParam(name="rout", value=0, unit=u.mas,
                                        description="Outer radius of the disk")
         self.params["q"] = oimParam(name="q", value=0, unit=u.one,
                                     description="Power-law exponent for the temperature profile")
@@ -244,20 +247,19 @@ class oimAsymTempGradient(oimAsymRadialPowerLaw):
         self._wl = np.array([0])  # constant value <=> achromatic model
         self._eval(**kwargs)
 
+    # FIXME: Possible bug due to r being in mas or rad?
+    # FIXME: Does pixsize need to be in rad or in mas? -> Check again, probably correct as is
     def _imageFunction(self, xx, yy, wl, t):
-        """Calculates a 2D temperature gradient profile via the dust-surface
-        density- and temperature profile"""
         r = np.sqrt(xx**2+yy**2)
         rin, rout = map(lambda x: self.params[x](wl, t), ["rin", "rout"])
         q, p = map(lambda x: self.params[x](wl, t), ["q", "p"])
         dist, inner_temp = map(lambda x: self.params[x](wl, t), ["dist", "Tin"])
+        rin_cm, rout_cm = map(lambda x: x*dist*self.params["rin"].unit.to(u.arcsec)*u.au.to(u.cm), [rin, rout])
         dust_mass = self.params["Mdust"](wl, t)*const.M_sun.value*1e3
         kappa_abs = self.params["kappa_abs"](wl, t)
-        rin_mas, rout_mas = map(lambda x: 1e3*x/dist, [rin, rout])
-        rin_cm, rout_cm = map(lambda x: x*self.params["rin"].unit.to(u.cm), [rin, rout])
 
         # NOTE: Temperature radial profile
-        temp_profile = inner_temp*(r / rin_mas)**q
+        temp_profile = inner_temp*(r / rin)**q
 
         # NOTE: Surface density radial profile
         if p == -2:
@@ -265,10 +267,11 @@ class oimAsymTempGradient(oimAsymRadialPowerLaw):
         else:
             f = ((rout_cm/rin_cm)**(p+2)-1)/(p+2)
             sigma_in = dust_mass/(2.*np.pi*f*rin_cm**2)
-        sigma_profile = self._azimuthal_modulation(xx, yy, wl, t)\
-            * (1 + sigma_in*(r / rin_mas)**p)
-        print(sigma_profile)
+        sigma_profile = sigma_in*(r / rin)**p
+
+        if self.asymmetric:
+            sigma_profile = sigma_profile*(1+self._azimuthal_modulation(xx, yy, wl, t))
         spectral_density = calculate_intensity(self.params, wl,
                                                kappa_abs, temp_profile,
                                                sigma_profile, rJy=True)
-        return np.nan_to_num(np.logical_and(r > rin_mas, r < rout_mas).astype(int)*spectral_density, nan=0)
+        return np.nan_to_num(np.logical_and(r > rin, r < rout).astype(int)*spectral_density, nan=0)
