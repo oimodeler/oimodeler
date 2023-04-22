dafa8d70cf1a5617c90a46a643b4300a7960f41b
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2023-04-22 14:00:27 +0200

    Update docstrings for `oimCustomComponents`

--- oimodeler/oimCustomComponents/oimStar.py
+++ oimodeler/oimCustomComponents/oimStar.py
@@ -23,10 +23,10 @@ class oimStar(oimComponentImage):
     ----------
     params : dict with keys of str and values of oimParam
         Dictionary of parameters.
-    _t : numpy.ndarray
-        Array of time values.
-    _wl : numpy.ndarray
-        Array of wavelength values.
+    _wl : array_like
+        Wavelengths.
+    _t : array_like
+        Times.
 
     Methods
     -------
@@ -37,7 +37,7 @@ class oimStar(oimComponentImage):
     _calc_stellar_flux(wl, t)
         Calculates the stellar flux from its distance and radius.
     _imageFunction(xx, yy, wl, t)
-        Creates a 2D-image with the stellar flux in Jansky at its centre
+        Calculates a 2D-image with the stellar flux in Jansky at its centre
         and only 0 for the rest.
     """
     name = "Physical Star"
@@ -61,13 +61,18 @@ class oimStar(oimComponentImage):
         self._t = np.array([0])
         self._eval(**kwargs)
 
-    def _convert_radius_to_parallax(self, orbital_radius: u.m, wl, t) -> u.mas:
+    def _convert_radius_to_parallax(self, orbital_radius: u.m,
+                                    wl: np.ndarray, t: np.ndarray) -> u.mas:
         """Calculates the parallax from the orbital radius.
 
         Parameters
         ----------
         orbital_radius : astropy.units.m
             The orbital radius.
+        wl : numpy.ndarray
+            Wavelengths.
+        t : numpy.ndarray
+            Times.
 
         Returns
         -------
@@ -82,12 +87,21 @@ class oimStar(oimComponentImage):
         distance = self.params["dist"](wl, t)*self.params["dist"].unit
         return u.rad.to(u.mas)*orbital_radius.to(u.m)/distance.to(u.m)
 
-    def _calc_stellar_radius(self, wl, t) -> u.m:
+    def _calc_stellar_radius(self, wl: np.ndarray, t: np.ndarray) -> u.m:
         """Calculates the stellar radius.
 
         Returns
         -------
         stellar_radius : astropy.units.m
+            The star's radius.
+        wl : numpy.ndarray
+            Wavelengths.
+        t : numpy.ndarray
+            Times.
+
+        Returns
+        -------
+        stellar_radius : u.m
             The star's radius.
         """
         luminosity, effective_temperature = map(
@@ -96,12 +110,19 @@ class oimStar(oimComponentImage):
                        / (4*np.pi*const.sigma_sb
                           * (effective_temperature*self.params["eff_temp"].unit)**4))
 
-    def _calc_stellar_flux(self, wl, t) -> u.Jy:
+    def _calc_stellar_flux(self, wl: np.ndarray, t: np.ndarray) -> u.Jy:
         """Calculates the stellar flux from its distance and radius.
+
+        Parameters
+        ----------
+        wl : numpy.ndarray
+            Wavelengths.
+        t : numpy.ndarray
+            Times.
 
         Returns
         -------
-        stellar_flux : astropy.units.Jy
+        stellar_flux : u.Jy
             The star's flux.
         """
         plancks_law = models.BlackBody(
@@ -115,9 +136,26 @@ class oimStar(oimComponentImage):
         # TODO: Check this calculation for the pi in the formulas
         return (spectral_radiance*stellar_radius_angular**2).to(u.Jy)
 
-    def _imageFunction(self, xx, yy, wl, t):
-        """Creates a 2D-image with the stellar flux in Jansky at its centre
-        and only 0 for the rest."""
+    def _imageFunction(self, xx: np.ndarray, yy: np.ndarray,
+                       wl: np.ndarray, t: np.ndarray) -> np.ndarray:
+        """Calculates a 2D-image with the stellar flux in Jansky at its centre
+        and only 0 for the rest.
+
+        Parameters
+        ----------
+        xx : numpy.ndarray
+            The x-coordinate grid
+        yy : numpy.ndarray
+            The y-coordinate grid
+        wl : numpy.ndarray
+            Wavelengths.
+        t : numpy.ndarray
+            Times.
+
+        Returns
+        -------
+        image : numpy.ndarray
+        """
         image = xx*0
         val = np.abs(xx)+np.abs(yy)
         idx = np.unravel_index(np.argmin(val), np.shape(val))
