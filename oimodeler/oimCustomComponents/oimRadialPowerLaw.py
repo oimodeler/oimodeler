Correct the `_azimuthal_modulation` of the power law and gradients

oimodeler/oimCustomComponents/oimRadialPowerLaw.py
@@ -116,7 +116,7 @@ class oimRadialPowerLaw(oimComponentImage):
         -------
         azimuthal_modulation : numpy.ndarray
         """
-        phi = self.params["phi"](wl, t) * self.params["phi"].unit.to(u.rad)
+        phi = self.params["phi"](wl, t)*self.params["phi"].unit.to(u.rad)
         return self.params["a"](wl, t)*np.cos(np.arctan2(yy, xx)-phi)
 
     def _image(self, xx: np.ndarray, yy: np.ndarray,
@@ -146,7 +146,7 @@ class oimRadialPowerLaw(oimComponentImage):
         """
         if self.asymmetric:
             return self._image(xx, yy, wl, t) * \
-                (1-self._azimuthal_modulation(xx, yy, wl, t))
+                (1+self._azimuthal_modulation(xx, yy, wl, t))
         return self._image(xx, yy, wl, t)
 
 

oimodeler/oimCustomComponents/oimTempGradient.py
@@ -319,7 +319,7 @@ class oimAsymTempGradient(oimRadialPowerLaw):
 
         sigma_profile = sigma_in*(np.sqrt(xx**2+yy**2) / rin)**(-p)
         if self.asymmetric_surface_density:
-            return sigma_profile*(1-self._azimuthal_modulation(xx, yy, wl, t))
+            return sigma_profile*(1+self._azimuthal_modulation(xx, yy, wl, t))
         return sigma_profile
 
     def _temperature_profile(self, r, wl, t):
@@ -432,7 +432,7 @@ class oimAsymTempGradient(oimRadialPowerLaw):
         """
         if self.asymmetric_image:
             return self._image(xx, yy, wl, t) * \
-                (1-self._azimuthal_modulation(xx, yy, wl, t))
+                (1+self._azimuthal_modulation(xx, yy, wl, t))
         return self._image(xx, yy, wl, t)
 
 