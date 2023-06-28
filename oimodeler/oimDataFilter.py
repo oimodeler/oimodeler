149b54b99131d6969480e6420516dd0a22eb104e
AnthonyMeilland <63512040+AnthonyMeilland@users.noreply.github.com>
2023-06-28 11:58:53 +0200

    added keepDataType filter + allowing to pass single filter to oimDataFilter instead of list

--- oimodeler/oimDataFilter.py
+++ oimodeler/oimDataFilter.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 """Data filtering/modifying"""
 import numpy as np
-
 from .oimData import _oimDataType, _oimDataTypeArr
-from .oimUtils import cutWavelengthRange
+from .oimUtils import cutWavelengthRange, getDataArrname,getDataType
 
-
+###############################################################################
 class oimDataFilterComponent(object):
     """Base class for data filter"""
     name = "Generic Filter"
@@ -43,7 +42,7 @@ class oimDataFilterComponent(object):
         for datai in [data[i] for i in idx]:
             self._filteringFunction(datai)
 
-
+###############################################################################
 class oimRemoveArrayFilter(oimDataFilterComponent):
     """Simple filter removing arrays by type"""
     name = "Remove array by type Filter"
@@ -60,7 +59,7 @@ class oimRemoveArrayFilter(oimDataFilterComponent):
             while len(np.where(np.array([t.name for t in data]) == arri)[0]) != 0:
                 data.pop(arri)
 
-
+###############################################################################
 class oimWavelengthRangeFilter(oimDataFilterComponent):
     """Filter for cutting wavelength range"""
     name = "Wavelength range Filter"
@@ -77,7 +76,7 @@ class oimWavelengthRangeFilter(oimDataFilterComponent):
         cutWavelengthRange(data, wlRange=self.params["wlRange"],
                            addCut=self.params["addCut"])
 
-
+###############################################################################
 class oimDataTypeFilter(oimDataFilterComponent):
     """ """
     name = "Filtering by datatype"
@@ -101,12 +100,51 @@ class oimDataTypeFilter(oimDataFilterComponent):
                 for datai in data:
                     if datai.name == dtypearr:
                         datai.data[dtype] *= 0
+                        
+###############################################################################
+class oimKeepDataType(oimDataFilterComponent):
+    """ """
+    name = "Keep datatype filter"
+    shortname = "KDT"
+    description = "Keep atatype that are listed: VIS2DATA, VISAMP..."
 
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.params["dataType"] = []
+        self.params["removeArrIfPossible"] = True
+        self._eval(**kwargs)
 
+    def _filteringFunction(self, data):
+        if type(self.params["dataType"]) != type([]):
+            self.params["dataType"] = [self.params["dataType"]]
+
+        dataType=self.params["dataType"]
+        #dataType=["VISAMP","VISPHI","T3PHI"]
+        arr0=np.array(["PRIMARY","OI_ARRAY","OI_WAVELENGTH","OI_TARGET"])
+        arr2Keep=np.unique(np.array([getDataArrname(dti) for dti in dataType]))
+
+        hduname=[hdu.name for hdu in data]
+
+        arr2remove=[]
+        for ihdu,hdunamei in enumerate(hduname):
+            if not(hdunamei in arr0 or hdunamei in arr2Keep):
+                   extver=data[ihdu].header["EXTVER"]
+                   arr2remove.append((hdunamei,extver))
+            elif hdunamei in arr2Keep:
+                dataTypesi=getDataType(hdunamei)
+                for dataTypeij in dataTypesi:
+                    if not(dataTypeij in dataType):
+                        data[ihdu].data[dataTypeij][:]= 0
+        for arr2removei in arr2remove:
+            data.pop(arr2removei)    
+
+###############################################################################
 class oimDataFilter(object):
     """Class for data filter stack"""
 
     def __init__(self, filters=[]):
+        if isinstance(filters,oimDataFilterComponent):
+            filters=[filters]
         self.filters = filters
 
     def applyFilter(self, data):
