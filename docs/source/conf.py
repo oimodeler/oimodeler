66d164a1820fc36ff5728efbad36640f23e7e686
AnthonyMeilland <63512040+AnthonyMeilland@users.noreply.github.com>
2023-07-12 10:09:41 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,7 @@ import oimodeler as oim
 
 print("Python version")
 print (sys.version)
-
+sys.path.insert(0,str(Path(__file__).parent.parent.parent ))
 sys.path.insert(0,str(Path(__file__).parent.parent.parent / "oimodeler"))
 sys.path.insert(0,str(Path("sphinxext").resolve()))
 
