5d70df11c24ffc65987800831608c91762a0f208
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2023-09-22 10:56:33 +0200

    Add logarithmic grid to `oimRadialRing`

--- oimodeler/oimCustomComponents/oimRadialRing.py
+++ oimodeler/oimCustomComponents/oimRadialRing.py
@@ -2,6 +2,8 @@ import numpy as np
 
 from ..oimParam import _standardParameters, oimParam
 from ..oimComponent import oimComponentRadialProfile
+from ..oimOptions import oimOptions
+from ..oimUtils import convert_distance_to_angle
 
 
 class oimRadialRing(oimComponentRadialProfile):
@@ -78,13 +80,16 @@ class oimRadialRing(oimComponentRadialProfile):
 
     @property
     def _r(self):
-        """Gets the radius."""
-        if False:
-            dout = self.params["dout"](self._wl, self._t)
-        else:
-            dout = self.params["dout"](1e99)
-        rmax = 4*dout/2.
-        return np.linspace(0, 1, self.params["dim"](self._wl, self._t))*rmax
+        """Gets the radial profile [mas]."""
+        rout = convert_distance_to_angle(
+                self.params["rout"].value, self.params["dist"].value)
+        # TODO: Check if this is needed here as the hankel transform already pads.
+        binning =  1 if oimOptions["FTbinningFactor"] is None\
+                else oimOptions["FTbinningFactor"]
+        rmax = binning*rout
+        if oimOptions["GridType"] == "linear":
+            return np.linspace(0, 1, self.params["dim"].value)*rmax
+        return np.logspace(0.0, np.log10(rmax), self.params["dim"].value)
 
     @_r.setter
     def _r(self, r: np.ndarray):
