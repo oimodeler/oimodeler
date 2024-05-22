6fb9da8cbfae46f1ab7f40f18700f609ef753e55
Anthony Meilland <63512040+AnthonyMeilland@users.noreply.github.com>
2024-05-22 11:33:23 +0200

    small changes before 0.85

--- oimodeler/__init__.py
+++ oimodeler/__init__.py
@@ -31,7 +31,7 @@ from .oimUtils import  _oimDataType, _oimDataTypeArr, _oimDataTypeErr
 np.seterr(invalid='ignore')
 proj.register_projection(oimAxes)
 
-__version__ = "0.8.1"
+__version__ = "0.8.5"
 
 # TODO: After pathlib change of all `oimodeler` modules, remove str here
 __pkg_dir__ = Path(inspect.getfile(inspect.currentframe())).parent
