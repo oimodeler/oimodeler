875d971af1b481ea6cc0386c1bd62d2143410d88
Marten B. Scheuck <martenscheuck@gmail.com>
2025-02-17 12:23:25 +0100

    Split the Lazareff models further

--- oimodeler/oimCustomComponents/oimStarHaloDisc.py
+++ oimodeler/oimCustomComponents/oimStarHaloDisc.py
@@ -11,12 +11,10 @@ class oimStarHaloGaussLorentz(oimComponentFourier):
     name = "Star and Halo component with Gauss-Lorentzian disk"
     shortname = "SHGL"
     elliptic = True
-    asymmetric = False
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self._ar, self._ak = None, None
-        self.ar, self.ak = None, None
         self.params["la"] = oimParam(
             name="la",
             value=0,
@@ -84,34 +82,13 @@ class oimStarHaloGaussLorentz(oimComponentFourier):
             free=False,
             description="Central wavelength of the instrument",
         )
-        self.params["lkr"] = oimParam(
-            name="lkr",
-            value=0,
-            unit=u.one,
-            mini=-1,
-            maxi=1,
-            description="Logarithm of the kernel/ring radius",
-            free=False,
-        )
-        self.params["skw"] = oimParam(**_standardParameters["skw"])
-        self.params["skwPa"] = oimParam(**_standardParameters["skwPa"])
-        if not self.asymmetric:
-            self.params["skw"].free = False
-            self.params["skwPa"].free = False
-
         self._wl = None  # None value <=> All wavelengths (from Data)
         self._t = [0]  # This component is static
         self._eval(**kwargs)
 
     def _vis_gauss_lorentz(self, xp, yp, rho, wl, t):
         la, flor = self.params["la"](wl, t), self.params["flor"](wl, t)
-        if self.shortname == "SHGL":
-            ak = 10**la
-        else:
-            lkr = self.params["lkr"](wl, t)
-            ak = np.sqrt(10 ** (2 * la) / (1 + 10 ** (-2 * lkr)))
-
-        xx = np.pi * ak * u.mas.to(u.rad) * rho
+        xx = np.pi * 10**la * u.mas.to(u.rad) * rho
         vis_gauss = np.exp(-(xx**2) / np.log(2))
         vis_lor = np.exp(-2 * xx / np.sqrt(3))
         return (1 - flor) * vis_gauss + flor * vis_lor
@@ -133,17 +110,17 @@ class oimStarHaloGaussLorentz(oimComponentFourier):
 
     def _image_gauss_lorentz(self, xx, yy, wl, t):
         flor = self.params["flor"](wl, t)
-        hlr = 10 ** self.params["la"](wl, t) if self.ak is None else self.ak
+        ak = 10 ** self.params["la"](wl, t)
         radius = np.hypot(xx, yy)
         image_gauss = (
             np.log(2)
-            / (np.pi * hlr**2)
-            * np.exp(-((radius / hlr) ** 2) * np.log(2))
+            / (np.pi * ak**2)
+            * np.exp(-((radius / ak) ** 2) * np.log(2))
         )
         image_lor = (
-            hlr
+            ak
             / (2 * np.pi * np.sqrt(3))
-            * (hlr**2 / 3 + radius**2) ** (-3 / 2)
+            * (ak**2 / 3 + radius**2) ** (-3 / 2)
         )
         return (1 - flor) * image_gauss + flor * image_lor
 
@@ -160,47 +137,46 @@ class oimStarHaloGaussLorentz(oimComponentFourier):
 class oimStarHaloIRing(oimStarHaloGaussLorentz):
     name = "Star and Halo component with Gauss-Lorentzian ring convolved disk"
     shortname = "SHGLR"
-    asymmetric = True
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.params["lkr"].free = True
+        self.params["lkr"] = oimParam(
+            name="lkr",
+            value=0,
+            unit=u.one,
+            mini=-1,
+            maxi=1,
+            description="Logarithm of the kernel/ring radius",
+            free=True,
+        )
+        self.params["skw"] = oimParam(**_standardParameters["skw"])
+        self.params["skwPa"] = oimParam(**_standardParameters["skwPa"])
+
         self._t = np.array([0])
         self._wl = None
         self._eval(**kwargs)
 
-    @property
-    def ar(self):
-        la, lkr = self.params["la"].value, self.params["lkr"].value
-        if self._ar is None:
-            self._ar = np.sqrt(10 ** (2 * la) / (1 + 10 ** (2 * lkr)))
-        return self._ar
-
-    @ar.setter
-    def ar(self, value):
-        self._ar = value
-
-    @property
-    def ak(self):
-        la, lkr = self.params["la"].value, self.params["lkr"].value
-        if self._ak is None:
-            self._ak = np.sqrt(10 ** (2 * la) / (1 + 10 ** (-2 * lkr)))
-        return self._ak
-
-    @ak.setter
-    def ak(self, value):
-        self._ak = value
+    def _vis_gauss_lorentz(self, xp, yp, rho, wl, t):
+        flor = self.params["flor"](wl, t)
+        la, lkr = self.params["la"](wl, t), self.params["lkr"](wl, t)
+        ak = np.sqrt(10 ** (2 * la) / (1 + 10 ** (-2 * lkr)))
+        xx = np.pi * ak * u.mas.to(u.rad) * rho
+        vis_gauss = np.exp(-(xx**2) / np.log(2))
+        vis_lor = np.exp(-2 * xx / np.sqrt(3))
+        return (1 - flor) * vis_gauss + flor * vis_lor
 
     def _visFunction(self, xp, yp, rho, wl, t):
         fh = self.params["fh"](wl, t)
         fs, fc = self.params["fs"](wl, t), self.params["fc"](wl, t)
         kc, ks = self.params["kc"](wl, t), self.params["ks"](wl, t)
+        la, lkr = self.params["la"](wl, t), self.params["lkr"](wl, t)
         wavelength_ratio = self.params["wl0"](wl, t) / wl
 
         skw, skwPa = self.params["skw"](wl, t), self.params["skwPa"](wl, t)
         skwPa *= self.params["skwPa"].unit.to(u.rad)
 
-        xx = 2 * np.pi * self.ar * u.mas.to(u.rad) * rho
+        ar = np.sqrt(10 ** (2 * la) / (1 + 10 ** (2 * lkr)))
+        xx = 2 * np.pi * ar * u.mas.to(u.rad) * rho
         vis_ring = j0(xx) + -1j * skw * np.cos(
             np.arctan2(xp, yp) - skwPa
         ) * j1(xx)
@@ -209,6 +185,24 @@ class oimStarHaloIRing(oimStarHaloGaussLorentz):
         vis_comp = fc * vis_gauss_lor * vis_ring * wavelength_ratio**kc
         divisor = (fs + fh) * wavelength_ratio**ks + fc * wavelength_ratio**kc
         return (vis_star + vis_comp) / divisor
+
+    def _image_gauss_lorentz(self, xx, yy, wl, t):
+        flor = self.params["flor"](wl, t)
+        la, lkr = self.params["la"](wl, t), self.params["lkr"](wl, t)
+        ak = np.sqrt(10 ** (2 * la) / (1 + 10 ** (-2 * lkr)))
+        radius = np.hypot(xx, yy)
+        image_gauss = (
+            np.log(2)
+            / (np.pi * ak**2)
+            * np.exp(-((radius / ak) ** 2) * np.log(2))
+        )
+        image_lor = (
+            ak
+            / (2 * np.pi * np.sqrt(3))
+            * (ak**2 / 3 + radius**2) ** (-3 / 2)
+        )
+        return (1 - flor) * image_gauss + flor * image_lor
+
 
     def _imageFunction(self, xx, yy, wl, t):
         fh = self.params["fh"](wl, t)
