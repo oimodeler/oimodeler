06d525c7c12c09c8bcdc2d8ad7a094a1285c7d2b
mbsck <code@mbscheuck.com>
2026-03-06 14:37:01 +0100

    feat(option): set grid.type = "logarithmic" by default

--- oimodeler/oimOptions.py
+++ oimodeler/oimOptions.py
@@ -19,12 +19,12 @@ backend = SimpleNamespace(active=None, available=[])
 fftw = SimpleNamespace(initialized=False)
 ft = SimpleNamespace(backend=backend, binning=None, padding=4, fftw=fftw)
 
-grid = SimpleNamespace(type="linear")
+grid = SimpleNamespace(type="logarithmic")
 model = SimpleNamespace(grid=grid)
 
-general =  SimpleNamespace(warning=True)
+general = SimpleNamespace(warning=True)
 
 # NOTE: The dictionary oimOption contains all the customizable option
 # of `oimodeler`.
 
-oimOptions = SimpleNamespace(ft=ft, model=model,general=general)
+oimOptions = SimpleNamespace(ft=ft, model=model, general=general)
