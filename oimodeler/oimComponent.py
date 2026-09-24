3bfaaf4da780d23ba7cc05d2578c3e7713db80da
Anthony Meilland <63512040+AnthonyMeilland@users.noreply.github.com>
2026-09-24 17:16:12 +0200

    correct printing bug in oimComponent

--- oimodeler/oimComponent.py
+++ oimodeler/oimComponent.py
@@ -567,7 +567,7 @@ class oimComponentFourier(oimComponent):
 
     def _imageFunction(self, xx, yy, wl, t):
         raise ValueError(
-            f"image function not implemented for {self.shortname}."
+            f"image function not implemented for {self.shortname}.\n"
             "Use the fromFT=True option to get a model image"
             " from the inverse Fourier Transform"
         )
