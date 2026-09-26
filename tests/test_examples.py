8fd7d080714593888ac7106253b37a8aa78a126b
mbsck <code@mbscheuck.com>
2026-09-26 11:57:47 +0200

    chore(tests): remove `papermill` and notebook tests

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -10,7 +10,6 @@ import subprocess
 import sys
 from pathlib import Path
 
-import papermill as pm
 import pytest
 
 EXAMPLES_DIR = Path(__file__).parent.parent / "examples"
@@ -31,19 +30,3 @@ def test_scripts(script: Path) -> None:
         cwd=script.parent,
     )
     assert result.returncode == 0
-
-
-@pytest.mark.slow
-# @pytest.mark.parametrize(
-#     "notebook",
-#     sorted([nb for nb in (EXAMPLES_DIR / "notebooks").rglob("*.ipynb")]),
-# )
-@pytest.mark.skip(reason="Test not yet finished.")
-def test_notebooks(notebook: Path) -> None:
-    """Tests all example Jupyter notebooks contained in examples/."""
-    output_notebook = notebook.with_suffix(".out.ipynb")
-    try:
-        pm.execute_notebook(str(notebook), str(output_notebook))
-        assert output_notebook.exists()
-    finally:
-        output_notebook.unlink(missing_ok=True)
